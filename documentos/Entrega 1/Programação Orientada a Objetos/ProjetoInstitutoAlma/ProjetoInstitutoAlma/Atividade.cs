9589dd73e9222b959dc68c2abd267c77853f4e19
Enzo <enzorib@hotmail.com>
2025-09-22 02:39:23 -0300

    Update POO

---
+++ documentos/Entrega 1/Programação Orientada a Objetos/ProjetoInstitutoAlma/ProjetoInstitutoAlma/Atividade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Atividade
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public DateTime Data { get; set; }
+
+    public Atividade(int id, string titulo, string descricao, DateTime data)
+    {
+        Id = id;
+        Titulo = titulo;
+        Descricao = descricao;
+        Data = data;
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"[Atividade] ID: {Id} | {Titulo} - {Descricao} | Data: {Data.ToShortDateString()}");
+    }
+}
